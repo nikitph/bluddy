1682fc76912d8ae8d09427faec3f21095d056eba
nikit <nikitph@gmail.com>
2015-04-01 22:15:46 -0700

    templating

--- bluddy.py
+++ bluddy.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g
 from emailassistant import EmailAssistant
+import simplejson
+from couchdb.design import ViewDefinition
+import flaskext.couchdb
 
 app = Flask(__name__)
 
@@ -10,19 +13,26 @@ def hello_world():
     email.emailer('alpha@nikitph.com', 'nikitph@gmail.com', 'test', 'test')
     return 'Hello World!'
 
+
 @app.route('/contactus')
 def contact_us_form():
     return render_template('contact.html')
 
+
 @app.route('/contactus', methods=['POST'])
 def contact_us_form_post():
+    # Dump request in DB
+    document = {'message': request.form}
+    g.couch.save(document)
     email = EmailAssistant()
     email.emailer('alpha@nikitph.com', 'nikitph@gmail.com', request.form['email'], request.form['message'])
     return render_template('confirmation.html', message='Successfully sent')
 
+
 @app.route('/scheduler')
 def scheduler_form():
     return render_template('scheduler.html')
+
 
 @app.route('/scheduler', methods=['POST'])
 def scheduler_form_post():
@@ -31,7 +41,15 @@ def scheduler_form_post():
     return render_template('scheduler.html')
 
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.config.update(
+        DEBUG = True,
+        COUCHDB_SERVER = 'http://127.0.0.1:5984/',
+        COUCHDB_DATABASE = 'docsdemo'
+    )
+    manager = flaskext.couchdb.CouchDBManager()
+    manager.setup(app)
+    manager.sync(app)
+    app.run(host='127.0.0.1', port=5000)
 
-# TODO insert some logging in here.
+    # TODO insert some logging in here.
