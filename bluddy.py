bbbcb1aa98cac03a036f540bd852977edf44dc3a
nikit <nikitph@gmail.com>
2015-04-08 15:26:28 -0700

    templating

--- bluddy.py
+++ bluddy.py
@@ -1,19 +1,10 @@
 from flask import Flask, render_template, request, g
 from emailassistant import EmailAssistant
-import simplejson as json
-from couchdb.design import ViewDefinition
-import flaskext.couchdb
-from valuetypes import Appointments
+from tinydb import TinyDB
 
 app = Flask(__name__)
 
 __author__ = 'bheenik'
-
-"""
-CouchDB permanent view
-"""
-occupied_dates = ViewDefinition('times', 'occupied_times',
-                                'function(doc) { if(doc.time) emit(doc.time,doc);}')
 
 
 @app.route('/')
@@ -30,7 +21,7 @@ def contact_us_form():
 def contact_us_form_post():
     # Dump request in DB
     document = {'message': request.form}
-    g.couch.save(document)
+    db.insert(document)
     email = EmailAssistant()
     email.emailer('alpha@nikitph.com', 'nikitph@gmail.com', request.form['email'], request.form['message'])
     return render_template('confirmation.html', message='Successfully sent')
@@ -43,13 +34,9 @@ def scheduler_form():
 
 @app.route('/scheduler', methods=['POST'])
 def scheduler_form_post():
-    schedule_data = Appointments(date=request.form['date'], time=request.form['time'], name=request.form['name'],
-                                 address=request.form['address'], phone=request.form['phone'],
-                                 emailaddress=request.form['email'])
-    id = schedule_data.store(g.couch)
+    document = {'message': request.form}
+    id = db.insert(document)
     print id
-    for i in occupied_dates(g.couch):
-        print(json.dumps(i, sort_keys=True, indent=4 * ' '))
     body = 'Appointment confirmed for ' + request.form['name'] + ' Resident of: ' + request.form['address'] + ' on ' + \
            request.form['date'] + ' at ' + request.form['time']
     email = EmailAssistant()
@@ -61,15 +48,7 @@ def scheduler_form_post():
 
 
 if __name__ == "__main__":
-    app.config.update(
-        DEBUG=True,
-        COUCHDB_SERVER='http://127.0.0.1:5984/',
-        COUCHDB_DATABASE='docsdemo'
-    )
-    manager = flaskext.couchdb.CouchDBManager()
-    manager.setup(app)
-    manager.add_viewdef(occupied_dates)
-    manager.sync(app)
+    db = TinyDB('db.json')
     app.run(host='127.0.0.1', port=5000)
 
     # TODO insert some logging in here.
